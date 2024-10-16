aaa58c2614adf35094be0227b2ce33da3920a87b
hus <inbox.of.hus@gmail.com>
2024-10-15 22:41:52 -0400

    Adding supplier functionality and intellij files

--- src/main/java/com/practice/java8/functional/main/BasicLambdas.java
+++ src/main/java/com/practice/java8/functional/main/BasicLambdas.java
@@ -1,13 +1,22 @@
 package com.practice.java8.functional.main;
 
 import com.practice.java8.functional.interfaces.Printable;
+import com.practice.java8.functional.interfaces.Retrievable;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class BasicLambdas {
 
     public static void main(String[] args) {
 
+        consumer();
+
+        supplier();
+
+    }
+
+    private static void consumer() {
         // Invoking Custom consumer and Java util consumer
         Printable<String> printable = (str) -> System.out.println(str);
         printable.print("Printable Lambda");
@@ -15,5 +24,14 @@ public class BasicLambdas {
         Consumer<String> consumer = System.out::println;
         consumer.accept("Printable consumer");
 
+    }
+
+    private static void supplier() {
+
+        Retrievable<Integer> retrievable = () -> {return 77;};
+        System.out.println(retrievable.retrieve());
+
+        Supplier<Integer> supplier = () -> {return 77;};
+        System.out.println(supplier.get());
     }
 }

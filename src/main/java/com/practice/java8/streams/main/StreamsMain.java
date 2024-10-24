3e7d156d36b3141750c53a9129f871b91c2ecb6a
Hus <37599216+iamhus@users.noreply.github.com>
2024-10-24 19:22:19 -0400

    Stream ops on custom objects

--- src/main/java/com/practice/java8/streams/main/StreamsMain.java
+++ src/main/java/com/practice/java8/streams/main/StreamsMain.java
@@ -1,5 +1,11 @@
 package com.practice.java8.streams.main;
 
+import com.practice.java8.streams.dao.Item;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StreamsMain {
@@ -8,6 +14,19 @@ public class StreamsMain {
 
         tryIntStreamOps();
 
+        tryStreamOfCustomObjOps();
+
+    }
+
+    private static void tryStreamOfCustomObjOps() {
+        List<Item> itemsList = Arrays.asList(
+                new Item(1, "Screw"),
+                new Item(2, "Nail"),
+                new Item(3, "Bolt"));
+
+        itemsList.stream()
+                .sorted(Comparator.comparing(Item::getName))
+                .forEach(System.out::print);
     }
 
     private static void tryIntStreamOps() {

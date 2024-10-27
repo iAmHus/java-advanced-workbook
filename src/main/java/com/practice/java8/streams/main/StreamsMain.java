dee2455d22e257b382e7e4a435b26e930fc6f522
Hus <37599216+iamhus@users.noreply.github.com>
2024-10-26 22:15:03 -0400

    Adding flatmap ops to streams

--- src/main/java/com/practice/java8/streams/main/StreamsMain.java
+++ src/main/java/com/practice/java8/streams/main/StreamsMain.java
@@ -5,8 +5,8 @@ import com.practice.java8.streams.dao.Item;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamsMain {
 
@@ -15,6 +15,19 @@ public class StreamsMain {
         tryIntStreamOps();
 
         tryStreamOfCustomObjOps();
+
+        tryFlatMap();
+
+    }
+
+    private static void tryFlatMap() {
+
+                Stream.of(
+                        Arrays.asList("a", "b"),
+                        Arrays.asList("a", "c"))
+                        .filter(l -> l.contains("c"))
+                        .flatMap(List::stream)
+                        .forEach(System.out::println);
 
     }
 
@@ -27,12 +40,12 @@ public class StreamsMain {
         itemsList.stream()
                 .sorted(Comparator.comparing(Item::getName))
                 .forEach(System.out::print);
+        System.out.println();
     }
 
     private static void tryIntStreamOps() {
-        IntStream.range(0,5)
-               .peek(System.out::println)
-               .average()
-               .ifPresent(System.out::println);
+        IntStream.range(0, 5)
+                .average()
+                .ifPresent(System.out::println);
     }
 }
